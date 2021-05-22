e528908122f509f44a0288c525f084b33ee06243
LGree <lgree@yandex.ru>
2021-05-22 10:48:52 +0300

    Update FakeStatusController.js

--- src/components/FakeStatusController.js
+++ src/components/FakeStatusController.js
@@ -1,10 +1,43 @@
-import {Paper} from "@material-ui/core";
+import React from 'react';
+import {FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel, Paper} from "@material-ui/core";
+import Switch from "react-bootstrap/Switch";
 
-const FakeStatusController = () => (
-    <div>
-        <Paper className="bg-light mb-3">
-            Status
-        </Paper>
-    </div>
-)
-export default FakeStatusController
+// const FakeStatusController = () => (
+export default function FakeStatusController() {
+    const [state, setState] = React.useState({
+        gilad: true,
+        jason: false,
+        antoine: true,
+    });
+
+    const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+    };
+
+
+    return (
+        <div>
+            <Paper className="bg-light mb-3">
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Статус</FormLabel>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={<Switch checked={state.gilad} onChange={handleChange} name="gilad" />}
+                            label="Миксер"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.jason} onChange={handleChange} name="jason" />}
+                            label="Холдер"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.antoine} onChange={handleChange} name="antoine" />}
+                            label="Печка"
+                        />
+                    </FormGroup>
+                    <FormHelperText>Подсказочка</FormHelperText>
+                </FormControl>
+            </Paper>
+        </div>
+    );
+}
+// export default FakeStatusController
